fbe3327c433a10790b1a04515f15e7881d972af6
Rajni Dua <rajni.dua14@gmail.com>
2021-10-20 17:49:32 +1030

    responsiveness added to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,94 +3,40 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 // Here we import the Navbar.css file to grant access to some additional styleNameNames
-//import "../styles/Navbar.css";
+import "../styles/Navbar.css";
 
 function Navbar() {
-  const styles = {
-    navbar: {
-      width: "100%",
-      border: "2px solid black",
-      position: "fixed",
-    },
-
-    maxWidthFlex: {
-      maxWidth: "1300px",
-      margin: "auto",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "space-between",
-      border: "2px solid pink",
-    },
-
-    li: {
-      /* border: "2px solid green", */
-    },
-
-    ul: {
-      listStyleType: "none",
-      border: "2px solid green",
-      margin: "auto",
-      display: "flex",
-    },
-
-    menuBtn: {
-      fontSize: "23px",
-      cursor: "pointer",
-      display: "none",
-    },
-
-    a: {
-      display: "inline",
-      color: "blue  ",
-      fontSize: "18px",
-      fontWeight: "500",
-      //marginLeft: "25px",
-      transition: "color 0.3s ease",
-      border: "2px solid red",
-    },
-  };
-
   return (
-    <nav style={styles.navbar}>
-      <div style={styles.maxWidthFlex}>
-        <div style={styles.logo}>
-          <a style={styles.a} href="https://github.com/rajnidua">
-            Portfo<span>lio.</span>
-          </a>
-        </div>
-        <div>
-          <ul style={styles.ul}>
-            <li styles={styles.li}>
-              <a style={styles.a} href="https://github.com/rajnidua">
-                Home
-              </a>
-            </li>
-            <li styles={styles.li}>
-              <a style={styles.a} href="https://github.com/rajnidua">
-                Services
-              </a>
-            </li>
-            <li>
-              <a style={styles.a} href="https://github.com/rajnidua">
-                Skills
-              </a>
-            </li>
-            <li styles={styles.li}>
-              <a style={styles.a} href="https://github.com/rajnidua">
-                Projects
-              </a>
-            </li>
-            <li styles={styles.li}>
-              <a style={styles.a} href="https://github.com/rajnidua">
-                Contact
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div style={styles.menuBtn}>
-          <FontAwesomeIcon icon={faBars} />
-        </div>
+    <nav className="navbar">
+      {/* <div className="max-width"> */}
+      <div className="logo">
+        <a href="https://github.com/rajnidua">
+          Ra<span>jni.</span>
+        </a>
       </div>
+
+      <ul className="menu">
+        <li>
+          <a href="https://github.com/rajnidua">Home</a>
+        </li>
+        <li>
+          <a href="https://github.com/rajnidua">Services</a>
+        </li>
+        <li>
+          <a href="https://github.com/rajnidua">Skills</a>
+        </li>
+        <li>
+          <a href="https://github.com/rajnidua">Projects</a>
+        </li>
+        <li>
+          <a href="https://github.com/rajnidua">Contact</a>
+        </li>
+      </ul>
+
+      <div className="menu-btn">
+        <FontAwesomeIcon icon={faBars} />
+      </div>
+      {/* </div> */}
     </nav>
   );
 }
