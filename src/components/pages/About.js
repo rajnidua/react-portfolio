e7930ba03e26df03e0991ba75a74a1d6fe4a8433
Rajni Dua <rajni.dua14@gmail.com>
2021-10-22 13:20:07 +1030

    updated styling for About

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,25 +4,25 @@ import "../../styles/About.css";
 export default function About() {
   return (
     <section className="about">
-      <div className="about-part-one">
+      <div className="about-part">
         I am a recent coding bootcamp graduate with a passion for developing
         scalable web applications and working across the full stack.
       </div>
-      <div className="about-part-two">
+      <div className="about-part margin-top margin-bottom">
         I focus on writing clean and modular code which is easily maintainable
         and scalable. My priority lies in taking a mobile first approach using
         functionalities like flexbox, grid and bootstrap and of course media
         queries.I make sure that the biggest impact features are worked on
         first.
       </div>
-      <div className="about-part-three">
+      <div className="about-part margin-top margin-bottom">
         I have built fully functional web applications by integrating third
         party APIs in Javascript, used both object-relational mapping with
         MySQL2(ORM)and object-document-mapping(ODM) with Mongoose.js,
         modularized applications following the Model-View-Controller(MVC)
         paradigm and much more ....
       </div>
-      <div className="about-part-four">
+      <div className="about-part margin-top margin-bottom">
         These days I am improvising my skills in REACT
       </div>
     </section>
