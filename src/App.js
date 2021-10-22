8cb9aa76de6768fe24d2fe2c1890caf493dba4c1
Rajni Dua <rajni.dua14@gmail.com>
2021-10-22 11:20:50 +1030

    created a project card component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Projects from "./components/Projects";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Projects />
     </div>
   );
 }
