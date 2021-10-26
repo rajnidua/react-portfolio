e459c5f09db4eae444500bfbf3384d2ec06e12f3
Rajni Dua <rajni.dua14@gmail.com>
2021-10-26 14:12:27 +1030

    fixed issue with footer

--- src/components/pages/Footer.js
+++ src/components/pages/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import "../../styles/Footer.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -13,30 +14,46 @@ import {
   faTwitter,
 } from "@fortawesome/free-brands-svg-icons";
 
+import { Button } from "@material-ui/core";
+
 import Rajni_Resume from "../../images/Rajni_Resume.pdf";
+import { dom } from "@fortawesome/fontawesome-svg-core";
 
 export default function Footer() {
   return (
     <footer className="footer">
       <div className="icon-display">
-        <a href="https://www.linkedin.com/in/rajnidua/">
+        {/*  <a href="https://www.linkedin.com/in/rajnidua/">
           <FontAwesomeIcon icon={faLinkedin} />
-        </a>
+        </a> */}
+        <Button href="https://www.linkedin.com/in/rajnidua/">
+          <FontAwesomeIcon icon={faLinkedin} />
+        </Button>
       </div>
       <div className="icon-display">
-        <a href="https://github.com/rajnidua">
+        {/* <a href="https://github.com/rajnidua">
           <FontAwesomeIcon icon={faGithub} />
-        </a>
+        </a> */}
+        <Button href="https://www.linkedin.com/in/rajnidua/">
+          <FontAwesomeIcon icon={faGithub} />
+        </Button>
       </div>
       <div className="icon-display">
-        <a href="mailto:rajni.dua14@gmail.com?subject=REGARDING WORK">
+        {/* <a href="mailto:rajni.dua14@gmail.com?subject=REGARDING WORK">
           <FontAwesomeIcon icon={faEnvelope} />
-        </a>
+        </a> */}
+
+        <Button href="https://www.linkedin.com/in/rajnidua/">
+          <FontAwesomeIcon icon={faEnvelope} />
+        </Button>
       </div>
       <div className="icon-display">
-        <a href={Rajni_Resume} download>
+        {/* <a href={Rajni_Resume} download>
           <FontAwesomeIcon icon={faFilePdf} />
-        </a>
+        </a> */}
+        <Button href="https://www.linkedin.com/in/rajnidua/">
+          <FontAwesomeIcon icon={faFilePdf} />
+        </Button>
       </div>
     </footer>
   );
